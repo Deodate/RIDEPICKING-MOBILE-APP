51c89d5ad7d2bc813162be17e7de1d52679fa3c7
Deodate <ajadeo04@gmail.com>
2024-11-15 15:40:49 +0200

    Admin

--- pickride/lib/auth/addCar.dart
+++ pickride/lib/auth/addCar.dart
@@ -20,11 +20,15 @@ class _AddCarScreenState extends State<AddCarScreen> {
         backgroundColor: const Color(0xFF0A395D),
         title: const Text(
           'Ride Admin',
-          style: TextStyle(fontWeight: FontWeight.bold),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white, // Set title text color to white
+          ),
         ),
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back,
+              color: Colors.white), // Set icon color to white as well
           onPressed: () {
             Navigator.pushReplacement(
               context,
@@ -33,137 +37,180 @@ class _AddCarScreenState extends State<AddCarScreen> {
           },
         ),
       ),
-      body: Padding(
+      body: Container(
+        color: const Color.fromARGB(
+            255, 4, 5, 75), // Set background color of the body
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 20),
-              const Center(
-                child: Text(
-                  'Add Cars',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 20),
+            const Center(
+              child: Text(
+                'Add Cars',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white, // Make title text white
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+
+            // Car Name Input
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Car Name',
+                labelStyle: TextStyle(
+                    fontFamily: 'Times New Roman', color: Colors.white),
+                hintStyle:
+                    TextStyle(color: Colors.white), // Optional hint color
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.white), // White underline
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.white), // White underline when focused
+                ),
+              ),
+              style: const TextStyle(
+                  color: Colors.white), // White text in input field
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a car name';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _carName = value!;
+              },
+            ),
+            const SizedBox(height: 16.0),
+
+            // Car Type Dropdown
+            DropdownButtonFormField<String>(
+              decoration: const InputDecoration(
+                labelText: 'Car Type',
+                labelStyle: TextStyle(
+                    fontFamily: 'Times New Roman', color: Colors.white),
+              ),
+              style: const TextStyle(
+                  color: Colors.black), // White text for dropdown
+              value: _carType,
+              items: ['SELECT', 'Picnic', 'Voiture']
+                  .map((type) => DropdownMenuItem<String>(
+                        value: type,
+                        child: Text(type),
+                      ))
+                  .toList(),
+              validator: (value) {
+                if (value == 'SELECT') {
+                  return 'Please select a car type';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  _carType = value!;
+                });
+              },
+            ),
+            const SizedBox(height: 16.0),
+
+            // Plack Input
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Plack',
+                labelStyle: TextStyle(
+                    fontFamily: 'Times New Roman', color: Colors.white),
+                hintStyle:
+                    TextStyle(color: Colors.white), // Optional hint color
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.white), // White underline
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.white), // White underline when focused
+                ),
+              ),
+              style: const TextStyle(
+                  color: Colors.white), // White text in input field
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a plack';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _plack = value!;
+              },
+            ),
+            const SizedBox(height: 32.0),
+
+            // Buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue, // Blue background
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // No border radius
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      // Handle form submission
+                      print('Car Name: $_carName');
+                      print('Car Type: $_carType');
+                      print('Plack: $_plack');
+                      // Perform any submission logic here
+                    }
+                  },
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
-              ),
-              const SizedBox(height: 40),
-
-              // Car Name Input
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Car Name',
-                  labelStyle: TextStyle(fontFamily: 'Times New Roman'),
+                const SizedBox(width: 16.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red, // Red background
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // No border radius
+                    ),
+                  ),
+                  onPressed: () {
+                    _formKey.currentState!.reset();
+                    setState(() {
+                      _carType = 'SELECT';
+                    });
+                  },
+                  child: const Text(
+                    'Cancel',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a car name';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _carName = value!;
-                },
-              ),
-              const SizedBox(height: 16.0),
-
-              // Car Type Dropdown
-              DropdownButtonFormField<String>(
-                decoration: const InputDecoration(
-                  labelText: 'Car Type',
-                  labelStyle: TextStyle(fontFamily: 'Times New Roman'),
-                ),
-                value: _carType,
-                items: ['SELECT', 'Sedan', 'SUV', 'Hatchback']
-                    .map((type) => DropdownMenuItem<String>(
-                          value: type,
-                          child: Text(type),
-                        ))
-                    .toList(),
-                validator: (value) {
-                  if (value == 'SELECT') {
-                    return 'Please select a car type';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() {
-                    _carType = value!;
-                  });
-                },
-              ),
-              const SizedBox(height: 16.0),
-
-              // Plack Input
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Plack',
-                  labelStyle: TextStyle(fontFamily: 'Times New Roman'),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a plack';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _plack = value!;
-                },
-              ),
-              const SizedBox(height: 32.0),
-
-              // Buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+              ],
+            ),
+            const Spacer(), // Pushes the footer to the bottom
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: Column(
                 children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue, // Blue background
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero, // No border radius
-                      ),
-                    ),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
-                        // Handle form submission
-                        print('Car Name: $_carName');
-                        print('Car Type: $_carType');
-                        print('Plack: $_plack');
-                        // Perform any submission logic here
-                      }
-                    },
-                    child: const Text(
-                      'Save',
-                      style: TextStyle(color: Colors.white),
-                    ),
+                  Text(
+                    'Joyce Mutoni',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
-                  const SizedBox(width: 16.0),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red, // Red background
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero, // No border radius
-                      ),
-                    ),
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                      setState(() {
-                        _carType = 'SELECT';
-                      });
-                    },
-                    child: const Text(
-                      'Cancel',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
+                  Text('© 2024 PickRide Inc.',
+                      style: TextStyle(color: Colors.white54, fontSize: 12)),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
